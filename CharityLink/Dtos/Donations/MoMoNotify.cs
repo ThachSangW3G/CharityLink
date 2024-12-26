431240e844d91eef714bca9a3fa8828d679bb756
trungtinhn <trungtinh1620@gmail.com>
2024-12-26 21:38:53 +0700

    set up payment Vnpay

---
+++ CharityLink/Dtos/Donations/MoMoNotify.cs
@@ -0,0 +1,13 @@
+﻿namespace CharityLink.Dtos.Donations
+{
+    public class MoMoNotify
+    {
+        public string PartnerCode { get; set; }
+        public string OrderId { get; set; }
+        public string RequestId { get; set; }
+        public long Amount { get; set; }
+        public int ResultCode { get; set; }
+        public string Message { get; set; }
+        public string ExtraData { get; set; }
+    }
+}
